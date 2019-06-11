54ea042e765cbe4af056a604c08e4d8d14fe9759
mayurQED42 <mayur.gajar@qed42.com>
2019-06-11 10:54:02 +0530

    event echo

--- event.php
+++ event.php
@@ -1,11 +1,10 @@
 <?php
-  echo "hello";
   $event1_from = $_GET['event1_from'];
   $event1_to = $_GET['event1_to'];
   $event2_from = $_GET['event2_from'];
-  $event2_to = $_GET['event2_to'];
-  echo $event1_from."<br>".$event1_to."<br>".$event2_from."<br>".$event2_to;
-  
-  
-  
+  $event2_to = $_GET['event2_to']; 
+  echo "<br>".$event1_from;
+  echo "<br>".$event1_to;
+  echo "<br>".$event2_from;
+  echo "<br>".$event2_to;
 ?>
